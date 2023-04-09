67806838a2abbef1dbc2d7389758f4bd85a52cec
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-04-09 12:31:31 -0300

    Criando outras paginas

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,18 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "O poderoso Gojo Satoru de Jujutsu Kaisen",
+      "description": `Descubra mais sobre Gojo Satoru, um dos personagens mais fortes e enigmáticos do anime Jujutsu Kaisen! 
+      Com habilidades incríveis de manipulação de energia, Gojo é temido por seus inimigos e admirado por seus aliados. 
+      Saiba mais sobre a história de fundo de Gojo, seus poderes e como ele se encaixa no enredo emocionante de Jujutsu Kaisen.
+      Não perca a chance de descobrir por que Gojo é um dos personagens mais icônicos e interessantes da série!`,
+      "photoCover":"https://dol.com.br/img/Artigo-Destaque/770000/Jujutsu-Kaisen_00772135_0_.png?xid=1924457"
+    },
+    {
+      "id":"2",
+      "title": "Nova Série anunciada no Disney +",
+      "description": "bla blabla",
+      "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+  ]
+  
